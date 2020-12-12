4ec1f082a88af487b8a4db5b9fbf4409226b1e0f
javier cruz <javier.cruz.odispo@hotmail.com>
2020-12-12 12:32:12 -0600

    save fot testing

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,41 +1,15 @@
 <script>
     import {onMount} from "svelte";
-    import {storage,setSecret,getSecret,delSecret} from "../storage";
+    import {storage,setSecret,getSecret,DataManager} from "../storage";
     export let userData;
     onMount(()=>{
-        storage.listFiles((a)=>{
-            storage.getFile(a,{decrypt:true})
-            .then(e=>JSON.parse(e))
-            .then(e=>{
-                console.log(e)
-            })
-            return true;
-        })
-        document.querySelector("form.set").addEventListener("submit",(e)=>{
-            e.preventDefault()
-            setSecret({title:e.target.querySelector("#title").value,message:e.target.querySelector("#message").value})
-        })
-        document.querySelector("form.get").addEventListener("submit",(e)=>{
-            e.preventDefault()
-            console.log(e.target.querySelector("#id").value)
-        })
-        document.querySelector("form.del").addEventListener("submit",(e)=>{
-            e.preventDefault()
-            console.log(e.target.querySelector("#id").value)
-        })
+        let testing = new DataManager(storage);
+        testing.all = (a)=>{
+            let {OK} = a;
+            if(OK){
+                console.log(OK)
+            }
+        }
     })
 </script>
 <div class="name">{userData.username}</div>
-<form class="set">
-    title:<input type="text" id="title">
-    message:<input type="text" id="message">
-    <input type="submit">
-</form>
-<form class="get">
-    id:<input type="text" id="id">
-    <input type="submit">
-</form>
-<form class="del">
-    id:<input type="text" id="id">
-    <input type="submit">
-</form>
--- src/storage.js
+++ src/storage.js
@@ -4,9 +4,42 @@ import {v4} from "uuid"
 
 export const storage = new Storage({ userSession });
 
+export class DataManager{
+    constructor(storage){
+        this.database = storage;
+    }
+    set all(a){
+        if(typeof a === "function"){
+            storage.listFiles((b)=>{a({OK:b});return true})
+            .then(e=>{a({Exit:e});})
+            .catch(e=>{a({Err:e});})
+        }else if(typeof a === "object"&&!Array.isArray(a)){
+            let {OK,Err,Exit} = a;
+            storage.listFiles(a=>{OK?OK(a):"";return true})
+            .then(e=>Exit?Exit(e):e)
+            .catch(e=>Err?Err(e):((a)=>{throw a})(e))
+        }
+    }
+    async del(a){
+        this.database.deleteFile(a)
+    }
+    set add(a){
+        let {name,content,OK,Error} = a;
+        if(name&&content){
+            this.database.putFile(name,content)
+            .then(e=>typeof OK === "function"?OK(e):e)
+            .catch(e=>typeof Error === "function"?Error(e):e)
+            .then(e=>{
+                console.log(e)
+            })
+        }
+    }
+}
+
 export const setSecret = (data)=>{
-    storage.putFile(v4(),JSON.stringify(data),{encrypt:true}).then(e=>{
-        console.log(e)
+    let id = v4();
+    storage.putFile(id,JSON.stringify(data),{encrypt:true}).then(e=>{
+        console.log(e,id)
     })
 }
 
