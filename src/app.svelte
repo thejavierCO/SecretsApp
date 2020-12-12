1b6dc04b2f28a158cec68620b4cdee019611757b
javier cruz <javier.cruz.odispo@hotmail.com>
2020-12-12 13:38:27 -0600

    save rama

--- src/app.svelte
+++ src/app.svelte
@@ -1,21 +1,4 @@
 <script>
-    import {authenticate} from "./auth";
-    import Auth from "./components/Auth.svelte";
-    export let userSession;
-    let logged = false;
-    if(userSession.isSignInPending()) {
-        userSession.handlePendingSignIn().then((userData) => {
-            window.history.replaceState({}, document.title, "/")
-            console.log(userData);
-        });
-    }else if(userSession.isUserSignedIn()) {
-        logged = true;
-    }
 </script>
 <main>
-    {#if logged===false}
-        <button on:click={authenticate}>Login BlockStack</button>
-    {:else if userSession.isUserSignedIn()}
-        <Auth userData={userSession.loadUserData()}/>
-    {/if}
 </main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import "./css/style.scss"
 import App from "./app.svelte";
-import {userSession} from "./auth"
 
 new App({
     target:document.querySelector("#app"),
-    props:{
-        userSession
-    }
+    props:{}
 })
