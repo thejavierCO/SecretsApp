45cc77cae8418131bb9923d6fe4e7a2e12d16290
javier cruz <javier.cruz.odispo@hotmail.com>
2020-12-16 18:47:21 -0600

    set service base 0

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./css/style.scss";
 import {Service} from "./tools/app";
-import {TwoFactorAuthenticate} from "./tools/2fa"
 
 const config = new Service();
 const app = config.makeApp("SecretsApp","https://cldup.com/JBYtQaqOZX.svg");
 
+console.log(app)
