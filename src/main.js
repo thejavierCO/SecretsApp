a5be4dfcd5ab75164cf89122f06a08deb26dd75d
javier cruz <javier.cruz.odispo@hotmail.com>
2020-12-19 00:47:19 -0600

    save testing

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import "./css/style.scss";
+import {AppBlockStack} from "./tools/BS"
+
+const control = new AppBlockStack();
+const app = control.CreateApp("SecretsApp","https://cldup.com/JBYtQaqOZX.svg");
---
+++ src/tools/2fa.js
@@ -0,0 +1,4 @@
+import Speakeasy  from "speakeasy";
+import {Auth} from "./BS"
+
+export class AuthTwoFa extends Auth{}
